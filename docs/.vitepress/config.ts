d845b79cc66dffb504d99b4b4595ce3fa8e803a1
unknown <huanghc@zenitera.com>
2024-05-15 10:49:47 +0800

    '1'

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,75 +18,87 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Week', link: '/vue/vue-pdf' }
+      { text: 'Code', link: '/code/vue/vue-pdf' },
+      { text: 'Engineering', link: '/engineering/本地开发更新' }
     ],
 
-    sidebar: [
-      {
-        text: 'Example',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' },
-        ]
-      },
-      {
-        text: 'Tools',
-        items: [
-          { text: '代码开发规范', link: '/tools/prettier' },
-        ]
-      },
-      {
-        text: 'Vue',
-        items: [
-          { text: 'vue-pdf', link: '/vue/vue-pdf' },
-        ]
-      },
-      {
-        text: 'Vite',
-        items: [
-          { text: 'index.html注入动态信息', link: '/vite/index.html注入动态信息' },
-        ]
-      },
-      {
-        text: 'Javascript',
-        items: [
-          { text: 'pdf水印', link: '/js/pdf水印' },
-        ]
-      },
-      {
-        text: 'CSS',
-        items: [
-          { text: '背景色问题', link: '/css/背景色问题' },
-        ]
-      },
-      {
-        text: 'NPM',
-        items: [
-          { text: '离线安装node-sass', link: '/npm/离线安装node-sass' },
-        ]
-      },
-      {
-        text: 'Miniprogram',
-        items: [
-          { text: '小程序开发tip', link: '/miniprogram/小程序开发tip' },
-        ]
-      },
-      {
-        text: 'Flutter',
-        items: [
-          { text: '学习笔记', link: '/flutter/note_01' },
-        ]
-      },
-      {
-        text: 'Week',
-        items: [
-          { text: '20230714', link: '/week/20230714' },
-          { text: '20230721', link: '/week/20230721' },
-          { text: '20230728', link: '/week/20230728' },
-          { text: '20230811', link: '/week/20230811' },
-        ]
-      },
-    ],
+    sidebar: {
+      '/code/':
+        [
+          {
+            text: 'Example',
+            items: [
+              { text: 'Markdown Examples', link: '/markdown-examples' },
+              { text: 'Runtime API Examples', link: '/api-examples' },
+            ]
+          },
+          {
+            text: 'Tools',
+            items: [
+              { text: '代码开发规范', link: '/code/tools/prettier' },
+            ]
+          },
+          {
+            text: 'Vue',
+            items: [
+              { text: 'vue-pdf', link: '/code/vue/vue-pdf' },
+            ]
+          },
+          {
+            text: 'Vite',
+            items: [
+              { text: 'index.html注入动态信息', link: '/code/vite/index.html注入动态信息' },
+            ]
+          },
+          {
+            text: 'Javascript',
+            items: [
+              { text: 'pdf水印', link: '/code/js/pdf水印' },
+            ]
+          },
+          {
+            text: 'CSS',
+            items: [
+              { text: '背景色问题', link: '/code/css/背景色问题' },
+            ]
+          },
+          {
+            text: 'NPM',
+            items: [
+              { text: '离线安装node-sass', link: '/code/npm/离线安装node-sass' },
+            ]
+          },
+          {
+            text: 'Miniprogram',
+            items: [
+              { text: '小程序开发tip', link: '/code/miniprogram/小程序开发tip' },
+            ]
+          },
+          {
+            text: 'Flutter',
+            items: [
+              { text: '学习笔记', link: '/code/flutter/note_01' },
+            ]
+          },
+          {
+            text: 'Week',
+            items: [
+              { text: '20230714', link: '/code/week/20230714' },
+              { text: '20230721', link: '/code/week/20230721' },
+              { text: '20230728', link: '/code/week/20230728' },
+              { text: '20230811', link: '/code/week/20230811' },
+            ]
+          },
+        ],
+      '/engineering/': [
+        {
+          text: 'Example',
+          items: [
+            { text: '本地开发更新', link: '/engineering/本地开发更新' },
+          ]
+        },
+      ]
+    },
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/HcIce' }
--- docs/index.md
+++ docs/index.md
@@ -5,21 +5,21 @@ layout: home
 hero:
   name: "Ice Maker"
   text: "doc for ice"
-  tagline: My great project tagline
-  actions:
-    - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
+  tagline: 雪糕赶快吃
+  # actions:
+  #   - theme: brand
+  #     text: Markdown Examples
+  #     link: /markdown-examples
+  #   - theme: alt
+  #     text: API Examples
+  #     link: /api-examples
 
-features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+# features:
+#   - title: Feature A
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature B
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+#   - title: Feature C
+#     details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
 ---
 
